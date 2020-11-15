4f2039e809dd850e130ebf330f8a1729783dc876
edzen12 <oichiev.edzen@gmail.com>
2020-11-15 20:31:01 +0600

    Project ecommerce Django/JS? commit for save code

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import *
 import json
+import datetime
 
 
 def store(request):
@@ -72,3 +73,32 @@ def updateItem(request):
         orderItem.delete()
 
     return JsonResponse("товар был добавлен", safe=False)
+
+
+def processOrder(request):
+    transation_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transation_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer=customer,
+                order=order,
+                address=data['shipping']['address'],
+                city=data['shipping']['city'],
+                state=data['shipping']['state'],
+                zipcode=data['shipping']['zipcode'],
+            )
+
+    else:
+        print('User is not logged')
+    return JsonResponse('Payment complete!', safe=False)
